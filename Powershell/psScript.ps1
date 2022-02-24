741808227dfe6ef5c1310fb00b886302a7eafb97
jab-1 <100281305+jab-1@users.noreply.github.com>
2022-02-24 09:40:00 +0000

    Add files via upload

---
+++ Powershell/psScript.ps1
@@ -0,0 +1,9 @@
+﻿mkdir myFolder
+cd myFolder
+ni myFile.txt
+echo "Hello World" > myFile.txt
+gc myFile.txt
+echo "Goodbye World" >> myFile.txt
+gc myFile.txt
+mkdir secondFolder
+mv "myFile.txt" -Destination "secondFolder"
---
+++ Powershell/psScriptTest1.ps1
@@ -0,0 +1,21 @@
+﻿echo "This is a hello world batch file"
+if (Get-Item -Path FolderA -ErrorAction Ignore){
+	echo "Folder already exists"
+}
+
+else{
+	echo "Creating folder"
+	md FolderA
+}
+
+cd FolderA
+echo "Hello World" > testFile.txt
+echo "New Line" >> testFile.txt
+echo "File after appending text"
+gc testFile.txt 
+
+echo "Adding content" >> testFile.txt
+echo "File after adding more text"
+cat testFile.txt 
+
+cd .. 
