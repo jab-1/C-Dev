512da5f137618241e1e77c092bce7bc65085514a
jab-1 <jabbar.khan@live.co.uk>
2022-02-28 23:15:38 +0000

    core loops code

---
+++ Core/Core concepts/Loops/Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using ControlFlowApp;
+using System;
+
+
+public class Program
+{
+
+
+    public static void Main(string[] args) {
+
+        var nums = new List<int> { 10, 6, 22, -7, 3 };
+
+        Console.WriteLine("Highest foreach: " + LoopTypes.HighestForEachLoop(nums));
+        Console.WriteLine("Highest for loop: " + LoopTypes.HighestForLoop(nums));
+        Console.WriteLine("Highest while loop: " + LoopTypes.HighestWhileLoop(nums));
+        Console.WriteLine("Highest do-while loop: " + LoopTypes.HighestDoWhileLoop(nums));
+
+    }
+}
+
