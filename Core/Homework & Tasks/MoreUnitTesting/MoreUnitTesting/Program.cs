b1df4ee248f18c880d946162c6b2a92ccc3d3843
jab-1 <jabbar.khan@live.co.uk>
2022-03-01 02:18:28 +0000

    finalised MoreUnitTesting

---
+++ Core/Homework & Tasks/MoreUnitTesting/MoreUnitTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoreTesting;
+public class Program{
+
+    public static void Main(String[] args)
+    {
+
+    }
+    public static string GetGrade(int mark)
+    {
+        return mark >= 65 ? (mark >= 85 ? "Distinction" : "Pass") : "Fail";
+    }
+
+   
+} 
---
+++ Core/Homework & Tasks/MoreUnitTesting/MoreUnitTestingTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using MoreTesting;
+using NUnit.Framework;
+
+
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Category("Boundary testing")]
+    [TestCase(64, "Fail")]
+    [TestCase(65, "Pass")]
+    [TestCase(84, "Pass")]
+    [TestCase(85, "Distinction")]
+
+    [Category("Edge testing")]
+    [TestCase(-1000, "Fail")]
+    [TestCase(1000, "Distinction")]
+
+    public void ReturnCorrectGrade(int grade, string expected)
+    {
+        Assert.That(Program.GetGrade(grade), Is.EqualTo(expected));
+    }
+}
