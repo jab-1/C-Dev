04b8d858f808f619385b54ebebd4bf1ce5a7f724
jab-1 <jabbar.khan@live.co.uk>
2022-03-01 01:40:28 +0000

    Operators_ControlFlow_Lab_Starter completed

---
+++ Core/Core concepts/Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System;
+namespace Op_CtrlFlow
+{
+    public class Exercises
+    {
+        public static bool MyMethod(int num1, int num2)
+        {
+            return num1 == num2 ? false : (num1 % num2) == 0;
+        }
+
+        // returns the average of the array nums
+        public static double Average(List<int> nums)
+        {
+            
+            double sum = 0;
+            if (nums.Count != 0)
+            {
+                foreach (double i in nums)
+                {
+                    sum = sum + i;
+                }
+                sum /= nums.Count;
+            }
+
+            return sum;
+        }
+
+        // returns the type of ticket a customer is eligible for based on their age
+        // "Standard" if they are between 18 and 59 inclusive
+        // "OAP" if they are 60 or over
+        // "Student" if they are 13-17 inclusive
+        // "Child" if they are 5-12
+        // "Free" if they are under 5
+        public static string TicketType(int age)
+        {
+            string ticketType = string.Empty;
+            switch (age)
+            {
+                case < 5:
+                    ticketType = "Free";
+                    break;
+                case >= 5 when age <= 12:
+                    ticketType = "Child";
+                    break;
+                case >= 13 when age <= 17:
+                    ticketType = "Student";
+                    break;
+                default:
+                    ticketType = "Standard";
+                    break;
+                case >= 60:
+                    ticketType = "OAP";
+                    break;               
+            }
+            return ticketType;
+        }
+
+        public static string Grade(int mark)
+        {
+            var grade = "";
+            if (mark >= 0 && mark <= 100) {
+                if (mark < 40)
+                {
+                    grade = "Fail";
+                }
+                else if (mark < 60)
+                {
+                    grade = "Pass";
+                }
+                else if (mark < 75) 
+                {
+                    grade = "Pass with Merit";
+                }
+                else if (mark <= 100)
+                {
+                    grade = "Pass with Distinction";
+                }
+            }
+            return grade;
+        }
+
+        public static int GetScottishMaxWeddingNumbers(int covidLevel)
+        {
+            if (covidLevel >= 0 && covidLevel <= 4)
+            {
+                switch (covidLevel)
+                {
+                    case 0:
+                        return 200;
+                    case 1:
+                        return 100;
+                    case 2:
+                        return 50;
+                    case 3:
+                        return 50;
+                    case 4:
+                        return 20;
+
+                }
+                return 0;
+            }
+            else {
+                Console.WriteLine("Value entered is not within acceptable bounds.");
+                return -1; 
+            }
+        }
+    }
+}
---
+++ Core/Core concepts/Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using Op_CtrlFlow;
+using System.Collections.Generic;
+
+namespace Op_CtrlFlow_Tests
+{
+    public class Exercises_Tests
+    {
+        // write unit test(s) for MyMethod here
+        [Category("Same numbers")]
+        [TestCase(0, 0, false)]
+        [TestCase(3, 3, false)]
+        [TestCase(4, 4, false)]
+
+        [Category("Not divisible")]
+        [TestCase(3, 2, false)]
+        [TestCase(3, 6, false)]
+        [TestCase(9, 4, false)]
+
+        [Category("Different number and divisble")]
+        [TestCase(4, 2, true)]
+        [TestCase(6, 3, true)]
+        [TestCase(5, 1, true)]
+
+        public void ReturnTrueIfDivisibleAndDifferentNumbers(int num1, int num2, bool expected) {
+            Assert.That(Exercises.MyMethod(num1, num2), Is.EqualTo(expected));
+        }
+
+
+        [Test]
+        public void Average_ReturnsCorrectAverage()
+        {
+            var myList = new List<int>() { 3, 8, 1, 7, 3 };
+            Assert.That(Exercises.Average(myList), Is.EqualTo(4.4));
+        }
+
+        [Test]
+        public void WhenListIsEmpty_Average_ReturnsZero()
+        {
+            var myList = new List<int>() { };
+            Assert.That(Exercises.Average(myList), Is.EqualTo(0));
+        }
+
+        [TestCase(100, "OAP")]
+        [TestCase(60, "OAP")]
+        [TestCase(59, "Standard")]
+        [TestCase(18, "Standard")]
+        [TestCase(17, "Student")]
+        [TestCase(13, "Student")]
+        [TestCase(12, "Child")]
+        [TestCase(5, "Child")]
+        [TestCase(4, "Free")]
+        [TestCase(0, "Free")]
+        public void TicketTypeTest(int age, string expected)
+        {
+            var result = Exercises.TicketType(age);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Category("Edge cases")]
+        [TestCase(75, "Pass with Distinction")]
+        [TestCase(100, "Pass with Distinction")]
+        [TestCase(60, "Pass with Merit")]
+        [TestCase(74, "Pass with Merit")]
+        [TestCase(40, "Pass")]
+        [TestCase(59, "Pass")]
+        [TestCase(39, "Fail")]
+        [TestCase(0, "Fail")]
+
+
+        public void GradeTest(int mark, string expected) { 
+            Assert.That(Exercises.Grade(mark), Is.EqualTo(expected));
+        }
+        [Category("0-4 normal values")]
+        [TestCase(0,200)]
+        [TestCase(1,100)]
+        [TestCase(2,50)]
+        [TestCase(3,50)]
+        [TestCase(4,20)]
+
+        [Category("Out of bounds")]
+        [TestCase(-4, -1)]
+        [TestCase(6, -1)]
+
+        public void ReturnCorrectAttendees(int level, int expected) {
+            Assert.That(Exercises.GetScottishMaxWeddingNumbers(level), Is.EqualTo(expected));       
+            
+        }
+
+    }
+}
