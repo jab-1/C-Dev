6795ede7e2519ffdd58d66b619d627973205c3fa
jab-1 <jabbar.khan@live.co.uk>
2022-03-04 13:23:42 +0000

    WPF commit

---
+++ Core/Core concepts/WPF/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+var trueOrFalse = double.TryParse("-150.00", out double num1);
+
+
+if(trueOrFalse == true)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
